ed6d424c443ad5e53445f6b42ab282874b9aeb60
Thomas Lewis <thomas.a.lewis@proton.me>
2025-01-10 07:07:01 -0700

    Updated Models

--- LEGO_Inventory/ImportData.cs
+++ LEGO_Inventory/ImportData.cs
@@ -40,8 +40,6 @@ public class ImportData
                     DateModified = DateTime.Parse(setInfo!["last_modified_dt"]!.ToString()),
                     NumParts = int.Parse(setInfo!["num_parts"]!.ToString()),
                     ReleaseYear = int.Parse(setInfo!["year"]!.ToString()),
-                    ManualPath = "",
-                    ManualURL = "",
                     OwnCount = 0,
                     BuildCount = 0
                 };
--- LEGO_Inventory/Models/SetBrick.cs
+++ LEGO_Inventory/Models/SetBrick.cs
@@ -1,5 +1,8 @@
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace LEGO_Inventory;
 
+[Table("SetBricks")]
 public class SetBrick
 {
     public string PartNum { get; set; }
