377f5756728189b82575189d559f7e6584cacddf
Thomas Lewis <thomas.a.lewis@proton.me>
2025-01-10 09:24:45 -0700

    Fixed missing field

--- LEGO_Inventory/ImportData.cs
+++ LEGO_Inventory/ImportData.cs
@@ -42,6 +42,7 @@ public class ImportData
                     DateModified = DateTime.Parse(setInfo!["last_modified_dt"]!.ToString()),
                     NumParts = int.Parse(setInfo!["num_parts"]!.ToString()),
                     ReleaseYear = int.Parse(setInfo!["year"]!.ToString()),
+                    ManualUrl = "",
                     OwnCount = 0,
                     BuildCount = 0
                 };
