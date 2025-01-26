3fdd043b1091ddfd9495932ba467474768eeed50
Michael Lewis <michael.james.lewis@outlook.com>
2025-01-26 00:56:58 -0600

    Finish postgres conversion

--- LEGO_Inventory/Database/InventoryContext.cs
+++ LEGO_Inventory/Database/InventoryContext.cs
@@ -11,14 +11,13 @@ public class InventoryContext : DbContext
     public DbSet<SetMinifig> SetMinifigs { get; set; }
     public DbSet<MinifigBrick> MinifigBricks { get; set; }
 
-    public string DbPath { get; }
+    public string DbConnection { get; }
 
 
     public InventoryContext()
     {
-        var folder = Environment.SpecialFolder.LocalApplicationData;
-        var path = Environment.GetFolderPath(folder);
-        DbPath = System.IO.Path.Join(path, "LegoInventory.db");
+
+        DbConnection = "";
     }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
@@ -81,6 +80,5 @@ public class InventoryContext : DbContext
     }
 
     protected override void OnConfiguring(DbContextOptionsBuilder options) =>
-        options
-            .UseSqlite($"Data Source={DbPath}");
+        options.UseNpgsql("<connection string>");
 }
